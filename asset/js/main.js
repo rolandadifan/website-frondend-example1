9c13214fe1513307164788e6e7f7e6d0905372ea
Rolanda <46946118+rolandadifan@users.noreply.github.com>
2020-10-06 14:24:08 +0700

    Add files via upload

---
+++ asset/js/main.js
@@ -0,0 +1,43 @@
+// owl carousel
+
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  items: 4,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+});
+$(".play").on("click", function () {
+  owl.trigger("play.owl.autoplay", [1000]);
+});
+$(".stop").on("click", function () {
+  owl.trigger("stop.owl.autoplay");
+});
+
+// smoote scroll
+var scroll = new SmoothScroll('a[href*="#"]');
+
+// scroll up
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
